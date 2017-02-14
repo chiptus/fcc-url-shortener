e08e70d833da53dabfbe80e77bdbca72ca8ae13a
chiptus <chiptus@gmail.com>
2017-02-14 11:38:41 +0200

    refactor url-shortener module

--- url-shortener.js
+++ url-shortener.js
@@ -3,37 +3,50 @@ const { DB_URL } = require('./config');
 const { getNextSequence } = require('./utils/db');
 const { isValidWebUri } = require('./utils/url');
 
+function getNextSequenceAndForwardDb(db) {
+  return getNextSequence(db)
+    .then(seq => ({ seq, db }));
+}
+
+function transformDbResult(originalUrl) {
+  return result => ({
+    originalUrl,
+    shortUrl: `http://localhost:3000/${result.ops[0].shortUrlId}`,
+  });
+}
+
+function insertUrlObject(originalUrl) {
+  return ({ seq, db }) =>
+    db.collection('urls')
+      .insertOne({
+        shortUrlId: seq,
+        originalUrl,
+      })
+      .then(transformDbResult(originalUrl));
+}
+
+
 function makeNewUrl(originalUrl = '') {
   if (!isValidWebUri(originalUrl)) {
     return Promise.reject(new Error('not valid url'));
   }
   return MongoClient.connect(DB_URL)
-    .then(db =>
-      getNextSequence(db)
-        .then(seq => ({ seq, db })))
-    .then(({ seq, db }) =>
-      db.collection('urls')
-        .insertOne({
-          shortUrlId: seq,
-          originalUrl,
-        })
-    ).then(result => ({
-      originalUrl,
-      shortUrl: `http://localhost:3000/${result.ops[0].shortUrlId}`,
-    }))
+    .then(getNextSequenceAndForwardDb)
+    .then(insertUrlObject(originalUrl))
     .catch((reason) => {
       console.error(reason);
     });
 }
 
+function findOriginalUrl(urlId) {
+  return db => db.collection('urls')
+    .findOne({ shortUrlId: urlId })
+    .then(doc => doc.originalUrl);
+}
+
 function getUrl(urlId) {
-  console.log("get url");
   return MongoClient.connect(DB_URL)
-    .then(db => {
-      return db.collection('urls')
-        .findOne({ shortUrlId: urlId })
-        .then(doc => doc.originalUrl);
-    });
+    .then(findOriginalUrl(urlId));
 }
 
 module.exports = {
