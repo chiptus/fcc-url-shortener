6d2a76da5b578a7e2879f3a2fbd23631fd14b10d
chiptus <chiptus@gmail.com>
2017-02-14 12:31:23 +0200

    configure mlab

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
+function buildDbUrl() {
+  const {
+    MONGOLAB_USER,
+    MONGOLAB_PASSWORD,
+  } = process.env;
+  if (MONGOLAB_USER) {
+    return `mongodb://${MONGOLAB_USER}:${MONGOLAB_PASSWORD}@ds139705.mlab.com:39705/urlshortener`;
+  }
+  return 'mongodb://localhost:27017/urlshortener';
+}
+
 module.exports = {
-  DB_URL: 'mongodb://localhost:27017/urlshortener',
+  DB_URL: buildDbUrl(),
 };
